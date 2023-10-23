2400bb9887fb6c7e33ff59c78b30ce69867a9c1e
Sebastianminecrat <135288080+Sebastianminecrat@users.noreply.github.com>
2023-10-23 15:55:04 -0600

    Add files via upload

---
+++ luly.js
@@ -0,0 +1,38 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("mikellino.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	
+}
+
+
+function draw() {
+	image(video, 0, 0, 600, 500);
+  
+}
+
+function reloi()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
